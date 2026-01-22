7348a29c1b5bd4a359435914252782038e528916
Khalil-Akasheh05 <khalilakasheh2005@gmail.com>
2026-01-22 21:37:44 +0300

    Add logo

--- src/Components/User/NavBar.jsx
+++ src/Components/User/NavBar.jsx
@@ -3,6 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./css/NavBar.css";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png"
 function NavBar() {
   const handleLogout = () => {
     localStorage.removeItem("user");
@@ -10,7 +11,9 @@ function NavBar() {
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
-        <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/home">
+        <img src={logo} alt="" className="navbar-logo"/>
+        </Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link as={Link} to="/home">
             Home
